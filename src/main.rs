5ce77e377c9aa419aecfac798b130f6d026fe955
Franklyn Sancho <franklyn.hs@gmail.com>
2022-10-10 14:52:23 -0300

    reparando o modo unsafe no código com variáveis e ownership

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,51 @@
 use std::io;
 
-static mut TOTAL_COINS: f64 = 0.0;
+fn operations_system(mut deposit: f64) {
+    loop {
+        println!("Digite um valor: ");
 
-fn insert_money() {
-    unsafe {
-        let coins = [2.00, 5.00, 10.00, 20.00, 50.00, 100.00];
+        let mut value = String::new();
 
-        loop {
-            println!("Insira quanto você deseja investir: ");
+        io::stdin()
+            .read_line(&mut value)
+            .expect("Falha ao let entrada");
 
-            let mut credit = String::new();
+        let value: f64 = value.trim().parse().expect("Valor inválido");
 
-            io::stdin()
-                .read_line(&mut credit)
-                .expect("Falha ao ler entrada");
-
-            let credit = credit.trim().parse().expect("valor inválidos");
-
-            if coins.contains(&credit) {
-                TOTAL_COINS = credit + TOTAL_COINS;
-                println!("Você inseriu {} e seu saldo é {}", credit, TOTAL_COINS)
-            } else if credit == 0.0 {
-                main()
-            } else {
-                println!("Ocorreu um erro")
-            }
+        if value > 0.0 {
+            deposit = value + deposit;
+            println!("Você depositou {} e seu saldo total é: {}", value, deposit)
+        } else if value == 0.0 {
+            get_money(deposit);
+        } else {
+            println!("Valor inválido")
         }
     }
 }
 
-fn get_money() {
-    unsafe {
-        loop {
-            println!("Digite quanto você deseja sacar: ");
+fn get_money(mut discredit: f64) {
+    loop {
+        println!("Digite quanto você deseja sacar: ");
 
-            let mut discredit = String::new();
+        let mut value = String::new();
 
-            io::stdin()
-                .read_line(&mut discredit)
-                .expect("Falha ao let entrada");
+        io::stdin()
+            .read_line(&mut value)
+            .expect("Falha ao let entrada");
 
-            let discredit: f64 = discredit.trim().parse().expect("Valor inválido");
+        let value: f64 = value.trim().parse().expect("Valor inválido");
 
-            if discredit < TOTAL_COINS {
-                TOTAL_COINS = TOTAL_COINS - discredit;
-                println!("Você sacou {discredit} e seu saldo é {TOTAL_COINS}")
-            } else {
-                println!("Ocorreu um erro");
-            }
+        if value < discredit {
+            discredit = discredit - value;
+            println!("Você sacou {} e seu saldo é {}", value, discredit)
+        } else {
+            println!("Ocorreu um erro");
         }
     }
 }
 fn main() {
+    let total_coins: f64 = 0.0;
+
     println!("Bem vindo ao seu internet banking");
     println!("escolha a sua opção: ");
     println!("1 - Inserir dinheiro");
@@ -63,9 +57,9 @@ fn main() {
     io::stdin().read_line(&mut option).expect("Erro de entrada");
 
     if option.contains("1") {
-        insert_money()
+        operations_system(total_coins)
     } else if option.contains("2") {
-        get_money()
+        get_money(total_coins)
     } else {
         println!("Opção inválida")
     }
